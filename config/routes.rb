4b5bdf88adce9e24dce7a42fb4679c84acfa455f
Olivia Tufte <ortufte@gmail.com>
2020-10-20 15:56:51 -0500

    update routes to reflect addition of loggs and medicines

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do 
-      resources :users
+      resources :users do 
+        resources :logs
+        resources :medicines
+      end
     end
   end
 end
+
